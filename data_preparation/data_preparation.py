7161cf6968bf140474b80ed64583b6f94f43bdd5
Haroun Shihab <haron.shihab88@gmail.com>
2018-06-30 13:11:40 +0200

    add data split

--- data_preparation/data_preparation.py
+++ data_preparation/data_preparation.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 
 def explode(df, index_cols, list_col):
     return df\
@@ -19,10 +19,18 @@ def prepare_binary_ads(df, keys, value):
         .reset_index()
 
 
+def train_val_split(df, seed, train_prop=0.8):
+    train, test = train_test_split(df, test_size=1-train_prop, random_state=seed)
+    ytrain = train.pop('cuisine')
+    xtrain = train.values
+    ytest = test.pop('cuisine')
+    xtest = test.values
+
+    return xtrain, ytrain, xtest, ytest
+
 if __name__ == "__main__":
     df = pd.read_json("../data/train.json")
     #out = explode(df[:5], ['id', 'cuisine'], 'ingredients')
     out = prepare_binary_ads(df[:5], ['cuisine', 'id'], 'ingredients')
     print(out.head())
     print(type(out))
-    print(out.shape)
