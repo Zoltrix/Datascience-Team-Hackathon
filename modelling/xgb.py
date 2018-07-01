060c051144bd7fe7adef685dafd96b38fc449630
Haroun Shihab <haron.shihab88@gmail.com>
2018-07-01 15:59:34 +0200

    call plot_learning_curve when running xgboost

--- modelling/xgb.py
+++ modelling/xgb.py
@@ -1,4 +1,6 @@
 from xgboost import XGBClassifier
+from sklearn.model_selection import learning_curve, ShuffleSplit
+import matplotlib.pyplot as plt
 
 def xgboost(xtrain, ytrain, **kwargs):
     xgb = XGBClassifier(**kwargs)
@@ -14,7 +16,10 @@ def eval(mdl, xtest, ytest):
 
 def run(xtrain, ytrain, xtest, ytest, **kwargs):
     mdl = xgboost(xtrain, ytrain, **kwargs)
-
+    cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
+    from modelling.learning_curve import plot_learning_curve
+    plot_learning_curve(mdl, 'xgb_lr_curve', xtrain, ytrain, (0., 1.), cv=None, n_jobs=-1)
+    plt.show()
     return eval(mdl, xtest, ytest)
 
 if __name__ == "__main__":
@@ -23,4 +28,4 @@ if __name__ == "__main__":
     df = load_raw_data("../data/train.json")
     xtrain, ytrain, xtest, ytest = prepare(df[:100], ['id', 'cuisine'], 'ingredients')
 
-    print(run(xtrain, ytrain, xtest, ytest))
+    print(run(xtrain, ytrain, xtest, ytest, n_jobs=-1))
